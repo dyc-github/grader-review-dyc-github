b3f260d1a02bdc584b76a0730d85ca17594e1462
David Choi <y.choi.david@gmail.com>
2023-11-06 11:51:05 -0800

    autograder initial implementation

--- grade.sh
+++ grade.sh
@@ -8,9 +8,56 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+# grading-area
+# student-submission
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
+submissionPath="student-submission"
+if ! [[ -e $submissionPath ]]
+then
+    echo "No submission found"
+    exit
+elif ! [[ -f "$submissionPath/ListExamples.java" ]]
+then
+    echo "Submission found but no file"
+    exit
+else
+    echo "Valid submission"
+fi
+
+cp -r $submissionPath/* grading-area
+cp -r *.java grading-area
+cp -r lib grading-area
+cd grading-area
+
+
+
+
+
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+javac -cp $CPATH *.java
+
+responseCode=$?
+if [[ $responseCode -ne 0 ]]
+then
+    echo "failed to compile error code: " $responseCode
+    exit
+else
+    echo "successfully compiled"
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore grading-area/TestListExamples > testOutput.txt
+stringsWithFailure=`grep "FAILURES!!!" testOutput.txt` 
+if [[ "$stringsWithFailure" == "" ]]
+then
+    echo "passed 100% good job"
+else
+    input_string=`grep "Tests run:*" testOutput.txt` 
+    numbers=($(echo "$input_string" | grep -oE '[0-9]+'))
+    echo ${numbers[1]}
+    echo "failed"
+fi
+
